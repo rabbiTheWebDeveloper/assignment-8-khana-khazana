6ad5271d8fb4c568053172ab5bbf89ce7fc180ad
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-04-29 11:04:13 +0600

    start adding

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-  await dbConnect();
+
   return (
     <html lang="en">
       <body className={inter.className}>
--- src/db/queries.js
+++ src/db/queries.js
@@ -1,23 +1,28 @@
 import { usersModel } from "@/models/users-modes";
+import { dbConnect } from "@/services/mongo";
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
 
 const { recipesModel } = require("@/models/recipes-modes");
 
 async function getAllRecipes() {
+  await dbConnect();
   const recipes = await recipesModel.find().lean();
   // console.log(recipes);
   return replaceMongoIdInArray(recipes);
 }
 
 async function getRecipeById(id) {
+  await dbConnect();
   const recipe = await recipesModel.findById(id).lean();
   return replaceMongoIdInObject(recipe);
 }
 async function createUser(user) {
+  await dbConnect();
   return await usersModel.create(user);
 }
 
 async function findByCredentials(formData) {
+  await dbConnect();
   const user = await usersModel.findOne(formData).lean();
   return replaceMongoIdInObject(user);
 }
