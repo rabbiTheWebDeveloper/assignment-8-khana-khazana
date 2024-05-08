b03a45da96aca4462bbf1dfafe86efacb9647bde
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-08 20:00:32 +0600

    Update getFullRoute.js

--- src/utils/getFullRoute.js
+++ src/utils/getFullRoute.js
@@ -1,7 +1,5 @@
 export const getFullRoute = () => {
-  // Check if window is defined (to ensure it's not server-side rendering)
   if (typeof window !== 'undefined') {
-    // Use window.location.href to get the full route
     const fullRoute = window.location.href;
     return fullRoute;
   }
