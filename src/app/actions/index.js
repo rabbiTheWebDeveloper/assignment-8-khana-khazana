5e14fb8a1444a8ea47580df772c38195cfb6da14
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-08 18:53:21 +0600

    Update index.js

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -29,7 +29,7 @@ async function addFavourite(favoriteId, authId) {
   } catch(error) {
       throw error;
   }
-  revalidatePath(`/`);
+  revalidatePath(`/details/${favoriteId}`);
 }
 export {
   registerUser,
