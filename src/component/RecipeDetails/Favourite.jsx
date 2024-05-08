0a7a8fe8ae3c9f6529bd063f42ef15eae773443c
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-08 20:20:15 +0600

    Update Favourite.jsx

--- src/component/RecipeDetails/Favourite.jsx
+++ src/component/RecipeDetails/Favourite.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import React, { useState, useTransition } from 'react';
 
 const Favourite = ({recipeId}) => {
-  const { auth } = useAuth();
+  const { auth, setAuth } = useAuth();
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const isInterested = auth && auth?.favourites?.find((id) => id === recipeId);
@@ -20,11 +20,14 @@ const Favourite = ({recipeId}) => {
     }
 };
 
-console.log(auth?.favourites);
+console.log(interested)
+console.log(recipeId)
+console.log(isInterested)
+console.log(auth)
   return (
-    <div  className={`flex gap-2  ${
-      interested && "bg-indigo-600 hover:bg-indigo-800"
-  }`}   onClick={() =>
+    <div  className={`flex gap-2 text-gray-600 cursor-pointer  ${
+      interested && " text-[#eb4a36] hover:text-[#eb4a36]"
+  }`}    onClick={() =>
       startTransition(() => {
           toggleInterest();
       })
@@ -44,7 +47,7 @@ console.log(auth?.favourites);
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
     </svg>
-    <span>{isInterested ? "Favourited" : "Favourite"}</span>
+    <span>{interested ? "Favourited" : "Favourite"}</span>
   </div>
   );
 };
