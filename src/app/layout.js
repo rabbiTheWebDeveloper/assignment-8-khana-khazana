dddc6059e79a52a383f261d12adbaa0e2cef8222
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-08 19:48:10 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/component/common/NavBar";
-import { dbConnect } from "@/services/mongo";
+
 import AuthProvider from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +9,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Recipe App",
   description: "The best Recipe App",
+  openGraph: {
+    images: [
+        {
+            url: "https://assignment-8-khana-khazana.vercel.app/api/og",
+            width: 1200,
+            height: 600,
+        },
+    ],
+},
 };
 
 export default async function RootLayout({ children }) {
